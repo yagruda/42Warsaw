c17bb2a6a94fced9d123aefe89ecc7d538dead40
yagruda <gggccclas@proton.me>
2025-10-05 13:11:56 +0200

    fix

--- Exam03/permutations/main.c
+++ Exam03/permutations/main.c
@@ -28,18 +28,6 @@ cab$
 cba$
 */
 
-<<<<<<< HEAD
-#include "unistd.h"
-#include "stdio.h"
-#include <stdlib.h>
-
-void swap(char* a, char* b)
-{
-	char temp = *a;
-	*a = *b;
-	*b = temp;
-}
-=======
 
 /*
  * EXECUTION EXAMPLE:
@@ -60,7 +48,6 @@ void swap(char* a, char* b)
 #include <stdio.h>
 #include <stdlib.h>
 
->>>>>>> 29fa19a64435e984ab9aeb8c2344b8611d61766c
 
 int ft_strlen(char* str)
 {
@@ -71,51 +58,6 @@ int ft_strlen(char* str)
 }
 void sort_str(char* str, int len);
 
-<<<<<<< HEAD
-void permutation(char* str, int len, int step)
-{
-	if (step == len)
-	{
-		printf("%s\n", str);
-		return;
-	}
-	else
-	{
-		for(int i = step; i <= len; i++)
-		{
-			swap(&str[i], &str[step]);
-			permutation(str,len, step+1);
-			swap(&str[i], &str[step]);
-		}
-	}
-	//sort_str(str,len);
-}
-
-void sort_str(char *str, int len)
-{
-    for (int i = 0; i < len - 1; i++)
-        for (int j = i + 1; j < len; j++)
-            if (str[i] > str[j])
-            {
-                char tmp = str[i];
-                str[i] = str[j];
-                str[j] = tmp;
-            }
-}
-
-int main(int argc, char** argv)
-{
-	if (argc != 2)
-		return 0;
-
-	char* input = argv[1];
-	int len = ft_strlen(input);
-
-	sort_str(input, len);
-
-	// Generate and print all permutations
-	permutation(input, len - 1, 0);
-=======
 void ft_bubble_sort(char* arr, int size)
 {
 	char temp;
@@ -168,7 +110,6 @@ int main(int argc, char** argv)
 	ft_bubble_sort(argv[1], len);
 	
 	permutation(len, argv[1], current, is_used, 0);
->>>>>>> 29fa19a64435e984ab9aeb8c2344b8611d61766c
 
 	free(is_used);
 	free(current);
