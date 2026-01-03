1a0d1b027364db822055e3ab0247a95684920079
yagruda <gggccclas@proton.me>
2026-01-03 13:12:15 +0100

    fix

--- CPP02/ex01/main.cpp
+++ CPP02/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yhruda <yhruda@student.42warsaw.pl>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/02 18:15:31 by yhruda            #+#    #+#             */
-/*   Updated: 2026/01/03 13:09:23 by yhruda           ###   ########.fr       */
+/*   Updated: 2026/01/03 13:11:45 by yhruda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,32 @@
 
 int main()
 {
-	Fixed a(5.1f);
-	Fixed b(5);
+	Fixed k(5.1f);
+	Fixed g(5);
 
-	std::cout << a.getRawBits() << std::endl;
-	std::cout << b.getRawBits() << std::endl;
+	std::cout << k.getRawBits() << std::endl;
+	std::cout << g.getRawBits() << std::endl;
 	
-	std::cout << a << std::endl;
-	std::cout << b << std::endl;
+	std::cout << k << std::endl;
+	std::cout << g << std::endl;
 
 	std::cout << "test " << (5<<8) << std::endl;
+
+	// subject test
+
+	Fixed a;
+	Fixed const b( 10 );
+	Fixed const c( 42.42f );
+	Fixed const d( b );
+	a = Fixed( 1234.4321f );
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
+	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
 
 	return 0;
 }
